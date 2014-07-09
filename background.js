5fe01e3439e8b852ab6a9475f374484692f0b250
yushulx <lingxiao1002@gmail.com>
2014-07-09 18:03:58 +0800

    improve jpeg quality

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.captureVisibleTab(null, function(src) {
+	chrome.tabs.captureVisibleTab(null, {format:"jpeg", quality:100}, function(src) {
 			var link = document.createElement('a');
 			link.download = "download.jpg";
 			link.href = src.replace("image/png", "image/octet-stream");
