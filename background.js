5e1522f89bffd577b3c6c83f77b8d8feeb232d8c
yushulx <lingxiao1002@gmail.com>
2014-07-11 17:58:33 +0800

    change logic

--- background.js
+++ background.js
@@ -1,8 +1,28 @@
 var screenshot = {
 	content : document.createElement("canvas"),
+	data : '',
 
 	init : function() {
 		this.initEvents();
+	},
+	
+	saveScreenshot : function() {
+		var image = new Image();
+		image.onload = function() {
+			var canvas = screenshot.content;
+			canvas.width = image.width;
+			canvas.height = image.height;
+			var context = canvas.getContext("2d");
+			context.drawImage(image, 0, 0);
+
+			// save the image
+			var link = document.createElement('a');
+			link.download = "download.png";
+			link.href = screenshot.content.toDataURL();
+			link.click();
+			screenshot.data = '';
+		};
+		image.src = screenshot.data; 
 	},
 	
 	initEvents : function() {
@@ -11,32 +31,23 @@ var screenshot = {
 				format : "png",
 				quality : 100
 			}, function(data) {
-				var image = new Image();
-				image.onload = function() {
-					var canvas = screenshot.content;
-					canvas.width = image.width;
-					canvas.height = image.height;
-					var context = canvas.getContext("2d");
-					context.drawImage(image, 0, 0);
+				screenshot.data = data;
+				
+				// send an alert message to webpage
+				chrome.tabs.query({
+					active : true,
+					currentWindow : true
+				}, function(tabs) {
+					chrome.tabs.sendMessage(tabs[0].id, {ready : "ready"}, function(response) {
+						if (response.download === "download") {
+							screenshot.saveScreenshot();
+						}
+						else {
+							screenshot.data = '';
+						}
+					});
+				}); 
 
-					// send an alert message to webpage
-					chrome.tabs.query({
-						active : true,
-						currentWindow : true
-					}, function(tabs) {
-						chrome.tabs.sendMessage(tabs[0].id, {ready : "ready"}, function(response) {
-							if (response.download === "download") {
-								// save the image
-								var link = document.createElement('a');
-								link.download = "download.png";
-								link.href = screenshot.content.toDataURL();
-								link.click();
-							}
-						});
-					}); 
-
-				};
-				image.src = data;
 			});
 		});
 	}
