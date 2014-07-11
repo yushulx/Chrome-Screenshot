8d50ca5524f7b6b6d682cec159148d0545597eda
yushulx <lingxiao1002@gmail.com>
2014-07-11 16:05:53 +0800

    a little bit of improvements

--- background.js
+++ background.js
@@ -1,8 +1,45 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.captureVisibleTab(null, {format:"jpeg", quality:100}, function(src) {
-			var link = document.createElement('a');
-			link.download = "download.jpg";
-			link.href = src.replace("image/png", "image/octet-stream");
-			link.click();
+var screenshot = {
+	content : document.createElement("canvas"),
+
+	init : function() {
+		this.initEvents();
+	},
+	
+	initEvents : function() {
+		chrome.browserAction.onClicked.addListener(function(tab) {
+			chrome.tabs.captureVisibleTab(null, {
+				format : "png",
+				quality : 100
+			}, function(data) {
+				var image = new Image();
+				image.onload = function() {
+					var canvas = screenshot.content;
+					canvas.width = image.width;
+					canvas.height = image.height;
+					var context = canvas.getContext("2d");
+					context.drawImage(image, 0, 0);
+
+					// send an alert message to webpage
+					chrome.tabs.query({
+						active : true,
+						currentWindow : true
+					}, function(tabs) {
+						chrome.tabs.sendMessage(tabs[0].id, {ready : "ready"}, function(response) {
+							if (response.download === "download") {
+								// save the image
+								var link = document.createElement('a');
+								link.download = "download.png";
+								link.href = screenshot.content.toDataURL();
+								link.click();
+							}
+						});
+					}); 
+
+				};
+				image.src = data;
+			});
 		});
-});
+	}
+};
+
+screenshot.init();
